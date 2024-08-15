422502f7fc901ec5e10155c8db1b1e78c40e4246
Mio Yasutake <mio.fujita.01@gmail.com>
2024-08-16 05:45:45 +0900

    update cf cli version to 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 cf api "$INPUT_CF_API"
 cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+cf install-plugin multiapps -f
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
 
-sh -c "cf7 $*"
+sh -c "cf $*"
